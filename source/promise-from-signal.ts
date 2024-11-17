04999a5076fdc06257afc206f5c4da1b4028cf5d
fregante <me@fregante.com>
2024-11-17 16:21:52 +0700

    Update dev dependencies (#10)

--- source/promise-from-signal.ts
+++ source/promise-from-signal.ts
@@ -1,3 +1,12 @@
+/**
+ * Returns a `Promise` that resolves or rejects when the signal is aborted. This lets you await a signal or combine it with other promises.
+ *
+ * The promise will use the abort reason as a resolved/rejected value.
+ *
+ * @param signal The signal to listen to. If you pass a controller, it will automatically extract its signal.
+ * @param options.rejects If `true`, the promise will reject instead of resolve when the signal is aborted.
+ * @returns
+ */
 export async function promiseFromSignal(
 	signal: AbortSignal | AbortController,
 	{rejects = false} = {},
@@ -7,6 +16,7 @@ export async function promiseFromSignal(
 	return new Promise((resolve, reject) => {
 		const fulfill = () => {
 			if (rejects) {
+				// eslint-disable-next-line @typescript-eslint/prefer-promise-reject-errors -- Passed as is
 				reject(trueSignal.reason);
 			} else {
 				resolve(trueSignal.reason);
