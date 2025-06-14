df5168585a224444e43419f438f795ebeacb5917
fregante <me@fregante.com>
2025-06-15 01:10:05 +0700

    `promiseRaceWithSignal` - Fix leak (#21)

--- source/promise-race-with-signal.ts
+++ source/promise-race-with-signal.ts
@@ -1,4 +1,4 @@
-import {promiseFromSignal} from './promise-from-signal.js';
+/* eslint-disable @typescript-eslint/prefer-promise-reject-errors, promise/prefer-catch, promise/prefer-await-to-then */
 
 /**
  * Like `Promise.race()`, but it accepts `AbortSignal` or `AbortController` instances.
@@ -8,19 +8,48 @@ import {promiseFromSignal} from './promise-from-signal.js';
  * @param signal The signal to listen to. If you pass a controller, it will automatically extract its signal.
  * @param options.abortRejects If `false`, the promise will resolve instead of reject when the signal is aborted.
  */
-async function promiseRaceWithSignal<T, AbortRejects extends boolean = true>(
+async function promiseRaceWithSignal<T>(
 	promise: Promise<T>,
 	signal: AbortSignal | AbortController | undefined,
-	{abortRejects = true as AbortRejects}: {abortRejects?: AbortRejects} = {},
-): Promise<AbortRejects extends false ? T | unknown : T> {
+	options: {abortRejects?: false}
+): Promise<T | unknown>;
+
+async function promiseRaceWithSignal<T>(
+	promise: Promise<T>,
+	signal: AbortSignal | AbortController | undefined,
+	options?: {abortRejects: true}
+): Promise<T>;
+
+async function promiseRaceWithSignal<T>(
+	promise: Promise<T>,
+	signal: AbortSignal | AbortController | undefined,
+	{abortRejects = true}: {abortRejects?: boolean} = {},
+): Promise<T | unknown> {
 	if (!signal) {
 		return promise;
 	}
 
-	return Promise.race([
-		promise,
-		promiseFromSignal(signal, {rejects: abortRejects}),
-	]) as Promise<AbortRejects extends false ? T | unknown : T>;
+	const trueSignal = signal instanceof AbortController ? signal.signal : signal;
+	if (abortRejects) {
+		trueSignal.throwIfAborted();
+	} else if (trueSignal.aborted) {
+		return trueSignal.reason;
+	}
+
+	return new Promise((resolve, reject) => {
+		const handleAbort = () => {
+			if (abortRejects) {
+				reject(trueSignal.reason);
+			} else {
+				resolve(trueSignal.reason);
+			}
+		};
+
+		trueSignal.addEventListener('abort', handleAbort, {once: true});
+		promise.then(resolve, reject).finally(() => {
+			trueSignal.removeEventListener('abort', handleAbort);
+		});
+	});
 }
 
 export {promiseRaceWithSignal};
